1c96383047f5cf6231719b8024f27d09e887901e
Jean-Sebastien Legare <js@sitelabs.ca>
2018-11-09 02:03:08 -0800

    [cluster-config] save to cluster-config.json in repo root

--- scripts/setup-tasks.sh
+++ scripts/setup-tasks.sh
@@ -4,6 +4,13 @@ HERE="$(readlink -f $(dirname "$0"))"
 
 set -ex
 
+OUTFILE="${OUTFILE:-cluster-settings.json}"
+
+if [[ -e "$OUTFILE" ]]; then
+    echo "file $OUTFILE already exists. aborting." >&2
+    exit 1
+fi
+
 # create ecs role
 if ! aws iam list-roles | grep -q reprod-ecs-role; then
     aws iam create-role \
@@ -22,7 +29,18 @@ done
 
 
 # default cluster creation
-aws ecs create-cluster --cluster-name 'reprod' > "cluster_config.json"
+# this can be run several times with the same name. oddly enough.
+#
+create_cluster_response=$(aws ecs create-cluster --cluster-name 'reprod')
+
+cluster_name=$("$HERE"/read_json_key cluster.clusterName <<<"$create_cluster_response")
+cluster_arn=$("$HERE"/read_json_key cluster.clusterArn <<<"$create_cluster_response")
+
+echo \
+"{
+\"cluster_name\": \"${cluster_name}\",
+\"cluster_arn\": \"${cluster_arn}\"
+}" | tee "${OUTFILE}"
 
 # create test task definition for align container
 
@@ -32,7 +50,3 @@ aws ecs register-task-definition \
     --cli-input-json file://"$HERE"/../tasks/align-task-definition.json \
     --execution-role-arn reprod-ecs-role \
     --task-role-arn reprod-ecs-role
-
-    
-    
-
