468c50cb3d2d4c0f37c113bdc0263ee988a4f1cc
Jean-Sebastien Legare <js@sitelabs.ca>
2018-11-09 02:03:46 -0800

    [platform] Package python platform module inside lambdas

--- scripts/update-lambda.py
+++ scripts/update-lambda.py
@@ -12,6 +12,9 @@ import logging
 import json
 import base64
 import fnmatch
+import errno
+import subprocess
+import shutil
 
 import boto3
 from botocore.exceptions import ClientError
@@ -19,6 +22,19 @@ from botocore.exceptions import ClientError
 
 EXCLUDES = [".metadata.json"]
 EXCLUDE_PATTERNS = ["*~"]
+
+REPO_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+
+PLATFORM_PKG = "bunnies"
+
+PLATFORM_SRC = os.path.join(REPO_ROOT, "platform", "python3.6")
+
+PLATFORM_EXTRA = [
+    {"src": os.path.join(REPO_ROOT, "network-settings.json"),
+     "dst": os.path.join(PLATFORM_PKG, "network-settings.json")},
+    {"src": os.path.join(REPO_ROOT, "cluster-settings.json"),
+     "dst": os.path.join(PLATFORM_PKG, "cluster-settings.json")}
+]
 
 def setup_logging():
     root = logging.getLogger()
@@ -33,7 +49,10 @@ def setup_logging():
 log = logging.getLogger("update-lambda")
 
 def zip_lambda_dir(ziproot, zipfd):
-    """zip files in folder, recursively, including empty folders, but excluding special files"""
+    """
+    zip files in folder, recursively, including empty folders, but
+    excluding special files
+    """
     def _add_entry(dirname, basename):
         if basename in EXCLUDES or any([fnmatch.fnmatchcase(basename, patt) for patt in EXCLUDE_PATTERNS]):
             return
@@ -46,6 +65,7 @@ def zip_lambda_dir(ziproot, zipfd):
     for root, dirs, files in os.walk(ziproot):
         for basename in files: _add_entry(root, basename)
         for basename in dirs: _add_entry(root, basename)
+
 
 def main():
     setup_logging()
@@ -64,16 +84,42 @@ def main():
         if e.errno != errno.EEXIST:
             raise
 
-    with tempfile.NamedTemporaryFile(suffix=".zip", prefix="update-lambda-", dir=args.workdir, delete=False) as tempfd:
-        with zipfile.ZipFile(tempfd, mode='w', compression=zipfile.ZIP_DEFLATED) as zipfd:
-            zip_lambda_dir(args.lambdadir, zipfd)
+    tmpfd = None
+    platform_tmpdir = None
+    try:
 
-    # boto wants base64 encoded string data
-    with open(tempfd.name, 'rb') as zipfd:
-        zipdata = zipfd.read()
-        zip64 = base64.encodestring(zipdata)
+        # make temp dir to store platform files
+        platform_tmpdir = tempfile.mkdtemp(suffix="_platform", prefix="reprod_", dir=args.workdir)
+        
+        # install platform tooling in lambda
+        log.debug("installing platform module in lambda build directory %s", platform_tmpdir)
+        try:
+            cmd = ['pip', 'install', '-t', platform_tmpdir, PLATFORM_SRC + "[lambda]"]
+            log.info("command: %s", cmd)
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as cpe:
+            log.error("Command %s exited with error %d. output: %s", cpe.cmd, cpe.returncode, cpe.output)
+            sys.exit(1)
 
-    os.unlink(tempfd.name)
+        with tempfile.NamedTemporaryFile(suffix=".zip", prefix="update-lambda-", dir=args.workdir, delete=False) as tmpfd:
+            with zipfile.ZipFile(tmpfd, mode='w', compression=zipfile.ZIP_DEFLATED) as zipfd:
+                zip_lambda_dir(platform_tmpdir, zipfd)
+
+                # add extra items
+                for entry in PLATFORM_EXTRA:
+                    zipfd.write(entry['src'], arcname=entry['dst'])
+                    log.info("added file %s", entry['dst'])
+
+                zip_lambda_dir(args.lambdadir, zipfd)
+
+        # boto wants base64 encoded string data
+        with open(tmpfd.name, 'rb') as zipfd:
+            zipdata = zipfd.read()
+            zip64 = base64.encodestring(zipdata)
+    finally:
+        # cleanup temp files
+        if tmpfd: os.unlink(tmpfd.name)
+        if platform_tmpdir: shutil.rmtree(platform_tmpdir)
 
     lambda_cli = boto3.client('lambda')
     iam_cli = boto3.resource('iam')
