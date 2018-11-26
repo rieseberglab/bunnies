9e55431f1d3611696a8f37cd131c99613a95b27e
Jean-Sébastien Légaré <jslegare@cs.ubc.ca>
2018-11-25 17:26:09 -0800

    [logging]

--- platform/python3.6/bunnies/__init__.py
+++ platform/python3.6/bunnies/__init__.py
@@ -19,7 +19,7 @@ def setup_logging(loglevel=logging.INFO):
     root.setLevel(loglevel)
     ch = logging.StreamHandler(sys.stderr)
     ch.setLevel(loglevel)
-    formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+    formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     root.addHandler(ch)
 
