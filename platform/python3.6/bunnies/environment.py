b68459ce9339941310fddecc176680f3621b2a66
Jean-Sebastien Legare <jslegare@cs.ubc.ca>
2019-06-26 13:19:39 -0700

    [environment] cmdline

--- platform/python3.6/bunnies/environment.py
+++ platform/python3.6/bunnies/environment.py
@@ -141,3 +141,52 @@ class ComputeEnv(object):
         for name, ddict in self.disks.items():
             dobj = ddict['obj']
             dobj.delete()
+
+
+def _create_env(envname='', **kwargs):
+    """create an environment and wait for it to be ready"""
+    myenv = ComputeEnv(envname)
+    myenv.create()
+    myenv.ready()
+
+def _delete_env(envname='', **kwargs):
+    """tear down an environment"""
+    myenv = ComputeEnv(envname)
+    myenv.delete()
+
+def main():
+    import argparse
+    import sys
+    import bunnies
+
+    bunnies.setup_logging()
+
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(help="sub-command help", dest="command")
+
+    subp = subparsers.add_parser("create", help="create a new environment and wait for it to be ready.",
+                                 description="Create a new environment and wait for it to be ready. "
+                                 "If the environment already exists, the program will wait for it to be"
+                                 " ready.")
+    subp.add_argument("envname", metavar="ENVNAME", type=str, help="the name of the new environment")
+
+    subp = subparsers.add_parser("delete", help="delete/teardown a compute environment")
+    subp.add_argument("envname", metavar="ENVNAME", type=str, help="the name of the environment")
+
+    args = parser.parse_args(sys.argv[1:])
+
+    if args.command is None:
+        sys.stderr.write("No subcommand specified.\n")
+        sys.stderr.write(parser.format_usage() + "\n")
+        sys.exit(1)
+
+    func = {
+        'create': _create_env(args),
+        'delete': _delete_env(args)
+    }.get(args.command)
+    retcode = func(args)
+    sys.exit(int(retcode) if retcode is not None else 0)
+
+if __name__ == "__main__":
+    main()
