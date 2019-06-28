950194d309f3d2f9f449cc47d5454d29f8357cd2
Jean-Sebastien Legare <jslegare@cs.ubc.ca>
2019-06-27 19:37:21 -0700

    [wip] job submission tests

--- platform/python3.6/bunnies/jobs.py
+++ platform/python3.6/bunnies/jobs.py
@@ -4,48 +4,64 @@ from .constants import PLATFORM
 import json
 import logging
 import os.path
+import glob
+from botocore.exceptions import ClientError
 
-def permissions_files():
+logger = logging.getLogger(__package__)
 
-    here = os.path.dirname(__file__
-    datadir = os.path.join
 
-jobs_ecs_trust = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ecs-tasks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
+def permissions_files(rolename):
+    here = os.path.dirname(__file__)
+    perms_dir = os.path.join(here, "data", "permissions")
+    matches = [permfile for permfile in glob.glob(os.path.join(perms_dir, "*-permissions.json"))
+               if os.path.basename(permfile).startswith(rolename)]
+    return matches
 
-def create_ecs_role():
-    ecs_role_name = PLATFORM + "-ecs-role"
+def setup_ecs_role():
+    ecs_role_name = PLATFORM + "-ecs"
+
+    jobs_ecs_trust = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ecs-tasks.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+
     client = boto3.client("iam")
-    found = client.get_role(RoleName=ecs_role_name)
-    if not found:
+
+    logger.info("creating IAM role %s", ecs_role_name)
+    try:
         client.create_role(Path='/',
                            RoleName=ecs_role_name,
                            AssumeRolePolicyDocument=json.dumps(jobs_ecs_trust))
-        client.put_role_policy(
-            RoleName=ecs_role_name,
-            PolicyName="")
+        logger.info("IAM role %s created", ecs_role_name)
+    except ClientError as clierr:
+        if clierr.response['Error']['Code'] == 'EntityAlreadyExists':
+            logger.info("using existing role %s", ecs_role_name)
+            pass
+        else:
+            raise
+
+    for pfile in permissions_files(PLATFORM + "-ecs"):
+        basename = os.path.basename(pfile)
+        noext = os.path.splitext(basename)[0]
+        logger.info("adding policy %s to role %s", os.path.basename(pfile), ecs_role_name)
+        with open(pfile, "rb") as pfd:
+            policy = pfd.read().decode('utf-8')
+            client.put_role_policy(RoleName=ecs_role_name,
+                                   PolicyName=noext,
+                                   PolicyDocument=policy)
+    return client.get_role(RoleName=ecs_role_name)
 
 
-# for fil in "$HERE"/../tasks/reprod-ecs-*-permissions.json; do
-#     aws iam put-role-policy \
-# 	--role-name reprod-ecs-role \
-# 	--policy-name "$(basename "$fil" -permissions.json)" \
-# 	--policy-document file://"$fil"
-# done
-
-
-def _make_jobdef(name, image, vcpu, memory):
+def _make_jobdef(name, jobroleArn, image, vcpu, memory):
     client = boto3.client('batch')
     jd = client.register_job_definition(
         jobDefinition=name,
@@ -54,8 +70,39 @@ def _make_jobdef(name, image, vcpu, memory):
             'image': image,
             'vcpu': 1,
             'memory': 128,
+            'jobRoleArn': jobroleArn,
+            'volumes': [
+                {
+                    'host': {
+                        'sourcePath': "/mnt/fsx1"
+                    },
+                    'name': 'scratchvol'
+                }
+            ],
+            'mountPoints': [
+                {
+                    'containerPath': "/data",
+                    'readOnly': False,
+                    'sourceVolume': 'scratchvol'
+                }
+            ],
+            'privileged': False,
+            'ulimits': [
+                { 'name': "CORE",
+                  'hardlimit': 0,
+                  'softlimit': 0
+                }
+            ],
+            'user': 'root'
+        },
+        retryStrategy={
+            'attempts': 1
+        },
+        timeout={
+            'attemptDurationSeconds': 1000
         }
     )
+
 
 def _test_submit(name, queue, jobdef, command):
     client = boto3.client('batch')
@@ -83,6 +130,9 @@ def _test_submit(name, queue, jobdef, command):
     )
 
 
+def _setup_jobs(**kwargs):
+    setup_ecs_role()
+
 def main():
     import argparse
     import sys
@@ -91,16 +141,9 @@ def main():
     bunnies.setup_logging()
     parser = argparse.ArgumentParser()
 
-    import os.path
-    
-    open(sys.pred
     subparsers = parser.add_subparsers(help="sub-command help", dest="command")
 
-    subp = subparsers.add_parser("create", help="create a new environment and wait for it to be ready.",
-                                 description="Create a new environment and wait for it to be ready. "
-                                 "If the environment already exists, the program will wait for it to be"
-                                 " ready.")
-    subp.add_argument("envname", metavar="ENVNAME", type=str, help="the name of the new environment")
+    subp = subparsers.add_parser("setup", help="setup entities needed for launching jobs")
 
     subp = subparsers.add_parser("delete", help="delete/teardown a compute environment")
     subp.add_argument("envname", metavar="ENVNAME", type=str, help="the name of the environment")
@@ -113,8 +156,7 @@ def main():
         sys.exit(1)
 
     func = {
-        'create': _create_env,
-        'delete': _delete_env
+        'setup': _setup_jobs,
     }.get(args.command)
     retcode = func(**vars(args))
     sys.exit(int(retcode) if retcode is not None else 0)
