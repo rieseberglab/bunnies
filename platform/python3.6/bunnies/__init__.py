dc05b95a670d019f44695f60b2d2b768f066de99
Jean-Sebastien Legare <js@sitelabs.ca>
2019-12-30 17:13:44 -0800

    [init] add get_blob_meta to bunnies main package scope

--- platform/python3.6/bunnies/__init__.py
+++ platform/python3.6/bunnies/__init__.py
@@ -21,5 +21,5 @@ from .graph import S3Blob, Transform, ExternalFile
 from .pipeline import build_pipeline
 from .data_import import DataImport
 from .environment import ComputeEnv
-from .utils import run_cmd
+from .utils import run_cmd, get_blob_meta
 from .logging_ import setup_logging
